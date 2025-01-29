2870d23726ab8c6d225d7ace922499ef137d0ec6
himel1625 <himelmia1625@gmail.com>
2025-01-30 01:43:15 +0600

    utilize the project sculpture and it's now ready to implement

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>E-Commerce-Team-Project</div>,
+  },
+]);
+export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import { RouterProvider } from 'react-router-dom';
+import './index.css';
+import router from './Routes/Router.jsx';
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
